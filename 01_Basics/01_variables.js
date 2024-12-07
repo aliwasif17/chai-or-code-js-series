64634122f249743c50aeef2987134904c02f7804
aliwasif17 <aliwasif00990@gmail.com>
2024-12-07 21:41:22 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+// const accountId = 11002;
+// let accountEmail = "ali@google.com";
+// var accountPassword = "009900"
+// accountCity = "Karachi";
+
+// accountId = 123 // const cannot be edit nor be replace
+// accountEmail = "wasif@google.com" let = let can be edit but not replace
+// accountPassword = "1234"// var can be edit and replace
+
+// var let const ko 1 sath decalre karney ka assan tariqa 
+// console.table([accountId, accountEmail, accountPassword, accountCity]);
