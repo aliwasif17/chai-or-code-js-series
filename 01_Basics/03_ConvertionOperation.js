ab2646a1fa9a774726df01828a81f1ccb5119804
aliwasif17 <aliwasif00990@gmail.com>
2024-12-09 17:01:29 +0000

    Convertion opertion data

--- 01_Basics/03_ConvertionOperation.js
+++ 01_Basics/03_ConvertionOperation.js
@@ -1,11 +1,11 @@
-// let score = "33"
+let score = "33"
 
 // console.log(typeof score); // check type of 
 // console.log(typeof score); // check type of 
 
 // convert  into Number
 
-// let valueInNumber = Number(score);
+let valueInNumber = Number(score);
 // console.log(typeof valueInNumber); //the type of NUmber
 // console.log(valueInNumber); //value is Nan ye jab ho gi Nan jab aap number k sath kohi Alphabet lagao gey 
 
@@ -21,9 +21,9 @@
 
 // convert into Boolean
 
-// let isloggedIN = 1
+let isloggedIN = 1
 
-// let BooleanIsLoggedIn = Boolean(isloggedIN)
+let BooleanIsLoggedIn = Boolean(isloggedIN)
 // console.log(typeof BooleanIsLoggedIn); // typesof Boolean
 // console.log(BooleanIsLoggedIn); // console.log Boolean
 
@@ -36,11 +36,44 @@
 
 // Convert into String
 
-// let someNumber = 33
+let someNumber = 33
 
 // let StringNumber = String(someNumber)
 // console.log(StringNumber) // Console.log 33
 // console.log(typeof StringNumber) // type of String
+
+// *********************** Operations ******************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue); //Answer is -3
+
+// console.log(3 + 3);
+// console.log(3 - 3);
+// console.log(3 * 3);
+// console.log(3 ** 2);
+// console.log(3 % 2);
+// console.log(3 / 2);
+
+let str1 = "wasif"
+let str2 = " ali"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // Answer 12
+// console.log("2" + "2");  // Answer 22
+// console.log("1" + 2 + 2); // Answer is 122 because Phele kohi bhi number string me ata hey to baki number bhi String ho jatey hey. 
+
+// console.log(+true) // Answer is 1
+// console.log(+"") // Answer is 0
+
+let gameCounter = 100
+gameCounter ++;
+// console.log(gameCounter) Answer is 101 because ++ yani increment hogaya 
+
+
+
 
 
 
