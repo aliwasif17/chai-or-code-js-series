54e842a65e3d2d6d019da02dffa5da403ff2fae4
aliwasif17 <aliwasif00990@gmail.com>
2024-12-09 16:32:42 +0000

    Convertion of data type

---
+++ 01_Basics/03_ConvertionOperation.js
@@ -0,0 +1,46 @@
+// let score = "33"
+
+// console.log(typeof score); // check type of 
+// console.log(typeof score); // check type of 
+
+// convert  into Number
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); //the type of NUmber
+// console.log(valueInNumber); //value is Nan ye jab ho gi Nan jab aap number k sath kohi Alphabet lagao gey 
+
+// Note
+
+// "33" => 33
+// "33abc" => NaN --->(Not a Number)
+// "33abc" => typeof Number
+// true => 1; false => 0
+// null => 0
+// undefined => undefined
+// ---------------------------------------------------------------------
+
+// convert into Boolean
+
+// let isloggedIN = 1
+
+// let BooleanIsLoggedIn = Boolean(isloggedIN)
+// console.log(typeof BooleanIsLoggedIn); // typesof Boolean
+// console.log(BooleanIsLoggedIn); // console.log Boolean
+
+// Note
+
+// 1 => true ; 0 => false;
+// "" => false
+//"Wasif" => true
+// ---------------------------------------------------------------------
+
+// Convert into String
+
+// let someNumber = 33
+
+// let StringNumber = String(someNumber)
+// console.log(StringNumber) // Console.log 33
+// console.log(typeof StringNumber) // type of String
+
+
+
